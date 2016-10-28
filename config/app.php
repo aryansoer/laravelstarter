c5af6a3a011f9737c06b9c37ad6e0bdfccc811ca
Behruz Isoev <isaev.behruz@gmail.com>
2016-10-28 12:14:35 +0000

    Installed Zizaco/entrust library

--- config/app.php
+++ config/app.php
@@ -166,7 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Zizaco\Entrust\EntrustServiceProvider::class,
         //
 
         /*
@@ -225,6 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
 
     ],
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,13 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('is_visitor')->default(0);
+            $table->integer('created_by')->unsigned()->nullable()->index();
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('created_by')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('restrict');
         });
     }
 
